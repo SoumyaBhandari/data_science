e8b3ddd53cb24a3077c96046c86763bc778bf263
SoumyaBhandari <52395218+SoumyaBhandari@users.noreply.github.com>
2019-07-02 07:59:52 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,21 @@
+#pryamid
+a= int(input("enter no"))
+b=1
+while a>=0:
+    print(a*" "+b*"* ")
+    a-=1
+    b+=1
+#reverse pyramid
+p=int(input("valur"))
+y=1
+while p>=0:
+    print(y*" "+ p*"* ")
+    y+=1
+    p-=1
+#square
+z=int(input("enter"))
+k=1
+while z>=0:
+    print("* "*4 )
+    k+=1
+    z-=1
